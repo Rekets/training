20fdb087dbae616e9cec5c2e40718450283c28b2
Rekets Alexander <73691955+Rekets@users.noreply.github.com>
2021-03-30 22:05:47 +0300

    work

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson_1
 {
@@ -10,6 +6,8 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, C#!");
+            Console.ReadLine();
         }
     }
 }
